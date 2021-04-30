41d515d70a512785964a6fb8deb661c012e65bda
dgc08 <digc0820@gmail.com>
2021-04-30 17:22:38 +0200

    added tuple exmple in example.py

--- example.py
+++ example.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
     print()
     lst = [1, 2, 3]
     dct = {"hello":"hi"}
+    tpl = (1, 2, 3)
     obj = datetime(year=2021, month=4, day=30)
-    write_pon("write_example.pon", lst, "lst", ["datetime"])
-    write_pon("write_example.pon", dct, "dct", ["datetime"])
+    write_pon("write_example.pon", lst, "lst")
+    write_pon("write_example.pon", tpl, "tpl")
+    write_pon("write_example.pon", dct, "dct")
     write_pon("write_example.pon", obj, "obj", ["datetime"])
     with open("write_example.pon", "r") as fl:
         print(read_pon(fl, ["datetime"]))
